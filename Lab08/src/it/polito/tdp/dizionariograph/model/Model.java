24a9436d4d2e78647c2be72d6562d6ea7cc77f4f
Salvatore <Salvatore@172.21.15.95>
2018-05-09 14:14:05 +0200

    Versione commentata

--- Lab08/src/it/polito/tdp/dizionariograph/model/Model.java
+++ Lab08/src/it/polito/tdp/dizionariograph/model/Model.java
@@ -1,6 +1,5 @@
 package it.polito.tdp.dizionariograph.model;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -26,10 +25,13 @@ public class Model {
 	public Graph <String, DefaultEdge> createGraph(int numeroLettere) {
 		
 		this.graph = new SimpleGraph <> (DefaultEdge.class);
+		// data la dimensione della parola ricevo la lista di parole che
+		// saranno i miei vertici
 		this.parole = dao.getAllWordsFixedLength(numeroLettere);
 
 		Graphs.addAllVertices(this.graph, parole);
 		
+		// aggiungo i collegamenti ai vertici: solo le parole simili
 		for (String s : parole)
 			for (String p : this.getAllSimiliarWord(s))
 				Graphs.addEdgeWithVertices(this.graph, s, p);
@@ -41,6 +43,8 @@ public class Model {
 		Set <String> similarWord = new HashSet <> ();
 		
 		for (String s : parole) {
+			// evito di considerare i cappi e di ripetere 
+			//il ciclo sulla stessa coppia di parole
 			if (!s.equals(parola) &&s.compareTo(parola) > 0) {
 				int count = 0;
 				for(int i = 0; i < parola.length(); i++) {
@@ -63,7 +67,6 @@ public class Model {
 		StringBuilder s = new StringBuilder ();
 		int maxDegree = 0;
 		String maxParola = "";
-		List <String> vicini = new ArrayList<> ();
 		
 		for (String vertice : this.graph.vertexSet())
 			if (this.graph.degreeOf(vertice) > maxDegree) {
@@ -71,7 +74,7 @@ public class Model {
 				maxParola = vertice;
 			}
 		
-		vicini = Graphs.neighborListOf(this.graph, maxParola);
+		List <String> vicini = Graphs.neighborListOf(this.graph, maxParola);
 		s.append("Grado massimo: " + maxDegree + "\nVertice: " + maxParola + "\nLista dei vicini: " + vicini);
 		
 		return s.toString();
